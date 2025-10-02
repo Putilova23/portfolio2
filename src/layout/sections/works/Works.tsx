78d7145ef8aae04a980f41b1d6e1be8560a13b3a
Alena <Alena1118@icloud.com>
2025-10-02 11:01:11 +0700

    Добавила контейнер

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -5,35 +5,38 @@ import { FlexWrapper } from '../../../components/FlexWrapper.tsx'
 import { Work } from './work/Work.tsx'
 import socialImg from './../../../assets/images/Rectangle 14.png'
 import timerImg from './../../../assets/images/Rectangle 16.png'
+import { Container } from '../../../components/Container.ts'
 
-const worksItems = ["All", "Landing Pace", "React", "Spa"]
+const worksItems = ['All', 'Landing Pace', 'React', 'Spa']
 
 export const Works = () => {
   return (
     <StyledWorks>
-       <SectionTitle>My Works</SectionTitle>
-
-      <Menu menuItems={worksItems}/>
-      <FlexWrapper justifyContent={"space-around"}>
-        <Work
-          title={"Social Network"}
-          src={socialImg}
-          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dignissimos ducimus earum eius enim eumexcepturi expedita fuga incidunt libero modi obcaecati omnis perferendis possimus, quibusdam quis quod sedvelit?"}
-
-        />
-
-
-        <Work title={"Timer"}
-              src={timerImg}
-              text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dignissimos ducimus earum eius enim eumexcepturi expedita fuga incidunt libero modi obcaecati omnis perferendis possimus, quibusdam quis quod sedvelit?"}
-        />
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>My Works</SectionTitle>
+        <Menu menuItems={worksItems} />
+        <FlexWrapper justifyContent={'space-around'}>
+          <Work
+            title={'Social Network'}
+            src={socialImg}
+            text={
+              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dignissimos ducimus earum eius enim eumexcepturi expedita fuga incidunt libero modi obcaecati omnis perferendis possimus, quibusdam quis quod sedvelit?'
+            }
+          />
+          <Work
+            title={'Timer'}
+            src={timerImg}
+            text={
+              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dignissimos ducimus earum eius enim eumexcepturi expedita fuga incidunt libero modi obcaecati omnis perferendis possimus, quibusdam quis quod sedvelit?'
+            }
+          />
+        </FlexWrapper>
+      </Container>
     </StyledWorks>
   )
 }
 
 const StyledWorks = styled.section`
-min-height: 100vh;
+  min-height: 100vh;
   background-color: #deefff;
 `
-
