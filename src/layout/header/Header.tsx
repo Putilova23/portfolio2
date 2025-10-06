a7789200cde366016ce65f2a9befdf548330a921
Dmitry Putilov <Dmit.Putilov@x5.ru>
2025-10-06 14:51:19 +0700

    save

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,24 +3,26 @@ import { Logo } from '../../components/logo/logo.tsx'
 import { Container } from '../../components/Container.ts'
 import { FlexWrapper } from '../../components/FlexWrapper.tsx'
 import { HeaderMenu } from './headerMenu/HeaderMenu.tsx'
+import { MobileMenu } from './mobileMenu/MobileMenu.tsx'
 
-const items = ["Home", "Skills", "Works", "Testimony", "Contacts"]
+const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contacts']
 
 export const Header = () => {
   return (
     <StyledHeader>
       <Container>
-        <FlexWrapper justifyContent={"space-between"} alignItems={"center"}>
-          <Logo/>
-          <HeaderMenu menuItems={items}/>
+        <FlexWrapper justifyContent={'space-between'} alignItems={'center'}>
+          <Logo />
+          <HeaderMenu menuItems={items} />
+          <MobileMenu menuItems={items} />
         </FlexWrapper>
       </Container>
     </StyledHeader>
   )
 }
 
-const StyledHeader = styled.header `
-background-color: rgba(31,31,32,0.9);
+const StyledHeader = styled.header`
+  background-color: rgba(31, 31, 32, 0.9);
   padding: 20px 0;
   position: fixed;
   top: 0;
--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -1,21 +1,25 @@
 import styled from 'styled-components'
 import { theme } from '../../../styles/Theme.ts'
 
-
-export  const HeaderMenu = (props: {menuItems: Array<string>}) => {
+export const HeaderMenu = (props: { menuItems: Array<string> }) => {
   return (
     <StyledHeaderMenu>
       <ul>
-
-        {props.menuItems.map((item, index)=>{
-          return <ListItem key={index}>
-            <Link href="">
-              {item}
-              <Mask><span>{item}</span></Mask>
-              <Mask><span>{item}</span></Mask>
-            </Link></ListItem>
+        {props.menuItems.map((item, index) => {
+          return (
+            <ListItem key={index}>
+              <Link href="">
+                {item}
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+              </Link>
+            </ListItem>
+          )
         })}
-
       </ul>
     </StyledHeaderMenu>
   )
@@ -23,16 +27,20 @@ export  const HeaderMenu = (props: {menuItems: Array<string>}) => {
 
 const StyledHeaderMenu = styled.nav`
   ul {
-  display: flex;
-  gap: 30px;
+    display: flex;
+    gap: 30px;
     justify-content: center;
+  }
+
+  @media ${theme.media.tablet} {
+    display: none;
   }
 `
 
 const Link = styled.a`
   font-family: 'Josefin Sans', sans-serif;
-  font-weight: 400; 
-  font-size: 30px; 
+  font-weight: 400;
+  font-size: 30px;
   text-align: center;
   color: transparent;
 `
@@ -45,7 +53,7 @@ const Mask = styled.span`
   height: 50%;
   overflow-y: hidden;
   color: ${theme.colors.accent};
-  
+
   & + & {
     top: 50%;
     span {
@@ -59,36 +67,32 @@ const ListItem = styled.li`
   position: relative;
 
   &::before {
-    content: "";
+    content: '';
     display: inline-block;
     height: 3px;
     background-color: ${theme.colors.accent};
-    
+
     position: absolute;
     top: 50%;
     left: -10px;
     right: -10px;
     z-index: 1;
-    
+
     transform: scale(0);
-    
-    
   }
-  
+
   &:hover {
     &::before {
       transform: scale(1);
     }
-    
-  ${Mask} {
-    transform: skewX(12deg) translateX(5px);
-    color: ${theme.colors.font};
-    
-    & + ${Mask} {
-      transform: skewX(12deg) translateX(-5px);
+
+    ${Mask} {
+      transform: skewX(12deg) translateX(5px);
+      color: ${theme.colors.font};
+
+      & + ${Mask} {
+        transform: skewX(12deg) translateX(-5px);
+      }
     }
   }
-    
-  }
 `
-
