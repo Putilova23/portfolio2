4582dd61223b487f36ad590033e9b39db7054982
Dmitry Putilov <Dmit.Putilov@x5.ru>
2025-10-06 15:56:46 +0700

    save

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,27 +3,30 @@ import photo from '../../../assets/images/Rectangle 4.png'
 import { FlexWrapper } from '../../../components/FlexWrapper.tsx'
 import { Container } from '../../../components/Container.ts'
 import { theme } from '../../../styles/Theme.ts'
+import { font } from '../../../styles/Common.tsx'
 
 export const Main = () => {
   return (
     <StyledMain>
       <Container>
- <FlexWrapper alignItems={"center"}  justifyContent={"space-between"} >
-   <div>
-     <SmallText>Hi There</SmallText>
-     <Name> I am Alena Putilova</Name>
-     <MainTitle>A Web Developer.</MainTitle>
-   </div>
-   <PhotoWrapper>
-     <Photo src={photo} alt="" />
-   </PhotoWrapper>
-
- </FlexWrapper>
+        <FlexWrapper
+          alignItems={'center'}
+          justifyContent={'space-between'}
+          wrap={'wrap'}
+        >
+          <div>
+            <SmallText>Hi There</SmallText>
+            <Name> I am Alena Putilova</Name>
+            <MainTitle>A Web Developer.</MainTitle>
+          </div>
+          <PhotoWrapper>
+            <Photo src={photo} alt="" />
+          </PhotoWrapper>
+        </FlexWrapper>
       </Container>
     </StyledMain>
   )
 }
-
 
 const StyledMain = styled.div`
   min-height: 100vh;
@@ -36,15 +39,20 @@ const PhotoWrapper = styled.div`
   z-index: 0;
 
   &::before {
-    content: "";
+    content: '';
     width: 360px;
     height: 470px;
-    border: 5px solid ${theme.colors. accent};
-    
+    border: 5px solid ${theme.colors.accent};
+
     position: absolute;
     top: -24px;
     left: 24px;
     z-index: -1;
+
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+    }
   }
 `
 
@@ -52,16 +60,30 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
 const MainTitle = styled.h1`
   font-weight: 400;
   font-size: 27px;
+
+  ${font({
+    weight: 400,
+    Fmax: 27,
+    Fmin: 20,
+  })}
 `
 
 const Name = styled.h2`
-  font-family: 'Josefin Sans', sans-serif;
-  font-weight: 700;
-  font-size: 50px;
+  ${font({
+    family: "'Josefin Sans', sans-serif",
+    weight: 700,
+    Fmax: 50,
+    Fmin: 36,
+  })}
   letter-spacing: 0.05em;
   margin: 10px 0;
 
@@ -69,12 +91,12 @@ const Name = styled.h2`
     position: relative;
 
     &::before {
-      content: "";
+      content: '';
       display: inline-block;
       width: 100%;
       height: 20px;
       background-color: ${theme.colors.accent};
-      
+
       position: absolute;
       bottom: 0;
       z-index: -1;
@@ -86,4 +108,3 @@ const SmallText = styled.h2`
   font-weight: 400;
   font-size: 14px;
 `
-
